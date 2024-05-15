a38f692e110738d8531015665ce1e3a2186e6715
Carolina Soares <m.carolina.soares1@gmail.com>
2024-05-15 14:16:49 -0300

    Update exer2.py

--- exercícios/aula7/exer2.py
+++ exercícios/aula7/exer2.py
@@ -1,9 +1,10 @@
-palindromo = ["ana", "pelo", "ovo", "reviver",
-"telhado", "abacate", "radar", "osso", "reler", "sol" , "rever", "sala"]
+palindromo = ["ana", "pelo", "ovo", "reviver", "a grama é amarga", "a mala nada na lama",
+"telhado", "abacate", "radar", "osso", "viver para viver", "reler", "ame o poema", "sol" , "rever", "sala"]
 
 contar_palindromo = 0
 for palavra in palindromo:
-    if palavra == palavra[::-1]:
+    palavra_sem_espacos = palavra.replace(" ", "")
+    if palavra_sem_espacos == palavra_sem_espacos[::-1]:
         print(f"{palavra} é um palíndromo")
         contar_palindromo +=1
     else:
