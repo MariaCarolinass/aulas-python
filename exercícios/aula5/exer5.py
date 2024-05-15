a5dffc935d1aacfbfc544bf9743ccc90def0eb91
Carolina Soares <m.carolina.soares1@gmail.com>
2024-05-15 11:40:47 -0300

    Update exer5.py

--- exercícios/aula5/exer5.py
+++ exercícios/aula5/exer5.py
@@ -1,10 +1,26 @@
-pessoas = []
+pessoasList = []
 
-for i in range(1, 6):
+for i in range(1, 3):
 	nome = str(input("Nome: "))
 	idade = int(input("Idade: "))
 	estadocivil = str(input("Estado civil: "))
 	pessoa = [nome, idade, estadocivil]
-	pessoas.append(pessoa)
+	pessoasList.append(pessoa)
 
-print(pessoas)
+pessoa_mais_velha = ""
+mais_velho = 0
+solteiro = 0
+casado = 0
+
+for pessoas in pessoasList:
+	if mais_velho < pessoas[1]:
+		mais_velho = pessoas[1]
+		pessoa_mais_velha = pessoas[0]
+	if pessoas[2] == "solteiro" or pessoas[2] == "solteira":
+		solteiro += 1
+	if pessoas[2] == "casado" or pessoas[2] == "casada":
+		casado += 1
+
+print(pessoasList)
+print(f"O mais velho da lista é {pessoa_mais_velha} com {mais_velho} anos")
+print(f"Temos {solteiro} pessoas solteiras e {casado} pessoas casadas na lista")
