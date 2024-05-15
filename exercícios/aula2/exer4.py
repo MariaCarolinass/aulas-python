5dffe8ae7e49272232675ecaf2ced57593ad9708
Carolina Soares <m.carolina.soares1@gmail.com>
2024-05-15 09:20:21 -0300

    Update exer4.py

--- exercícios/aula2/exer4.py
+++ exercícios/aula2/exer4.py
@@ -3,8 +3,8 @@ b = int(input("Qual o valor de b: "))
 c = int(input("Qual o valor de c: "))
 
 delta = b ** 2 - 4 * a * c
-x1 = (- b + delta ** (1/2)) / 2 * a 
-x2 = (- b - delta ** (1/2)) / 2 * a
+x1 = (- b + delta ** (1/2)) / (2 * a)
+x2 = (- b - delta ** (1/2)) / (2 * a)
 
 print(delta)
 print(x1)
